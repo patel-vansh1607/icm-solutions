441e8a46ab58aab935e353b1721293b8a16f016d
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-03 11:35:18 +0300

    adding navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import IntroComponent from './components/Intro';
 import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <IntroComponent />
     </div>
   );
 }
