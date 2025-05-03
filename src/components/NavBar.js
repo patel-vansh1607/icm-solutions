294c4fb6096166c0cf49f3e568c8247ad81db49e
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-03 11:12:46 +0300

    adding properties of nav if scrolled

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,8 +3,14 @@ import "../styles/Navbar.css"
 const NavBar = () => {
     return(
         <div className="main-div">
-            <nav>
-                
+            <nav
+            className={`fixed w-full z-50 transition-all duration-300 ${
+                isScrolled 
+                  ? 'bg-white shadow-md py-2' 
+                  : 'bg-transparent py-4'
+              }`}
+              >
+
             </nav>
         </div>
     )
