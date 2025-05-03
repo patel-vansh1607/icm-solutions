1fae06414ff685a535e9c0f7c99b8fedc65690b8
Esha Patel <Patellesha11@gmail.com>
2025-05-03 13:26:18 +0300

    added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import IntroComponent from './components/Intro';
 import NavBar from './components/NavBar';
+import ContactUs from "./pages/ContactUs"
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <IntroComponent />
+      <ContactUs />
+
     </div>
   );
 }
