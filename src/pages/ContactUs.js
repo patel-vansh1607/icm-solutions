84c6b1bcae7aa11004fe2173e5cd997763166061
Esha Patel <Patellesha11@gmail.com>
2025-05-03 13:09:06 +0300

    added src

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -28,6 +28,14 @@ const ContactUs = () => {
       <div className="contact-bottom">
         <div className="map-cont">
             <h2>Our Location</h2>
+            <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3975.823928953264!2d36.82194627500289!3d-1.2920653356347905!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f10db2191f029%3A0xcef42b2dd9286ed9!2sNairobi%2C%20Kenya!5e0!3m2!1sen!2ske!4v1682248064326!5m2!1sen!2ske"
+            width="100%"
+            height="300"
+            style={{ border: 0 }}
+            allowFullScreen=""
+            loading="lazy"
+          ></iframe>
         </div>
       </div>
 
