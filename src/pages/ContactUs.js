e712b0289d3532044cba032af4f92197c2f59160
Esha Patel <Patellesha11@gmail.com>
2025-05-04 12:41:12 +0300

    added

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -26,7 +26,7 @@ const ContactUs = () => {
             <input type="text" placeholder="Your Name" required />
             <input type="email" placeholder="Your Email" required />
             <textarea placeholder="Your Message" required></textarea>
-            <button type="submit">Send</button>
+            <button type="submit">Submit</button>
           </form>
         </div>
       </div>
