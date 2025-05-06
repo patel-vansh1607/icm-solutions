aadd4875fd3d0412c8545b33fd9101ef0cc96e18
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-06 11:18:12 +0300

    created file again

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+import NavBar from './components/NavBar';
+import IntroComponent from './components/Intro';
+import AboutUs from './pages/AboutUs';
+import ContactUs from './pages/ContactUs';
+import Terms from './pages/Terms';
+import Faq from './pages/Faq';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
