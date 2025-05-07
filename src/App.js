057de83a5a9ad6bcd6d71c944a81b7a4baea1dce
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-07 18:33:26 +0300

    added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import './App.css'
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+import './App.css';
+import Footer from './components/Footer';
+import IntroComponent from './components/Intro';
+import Navbar from './components/NavBar';
+import NavBar from './components/NavBar';
+import SeedFeatures from './components/SeedFeatures';
+import SeedGrid from './components/seeds/SeedGrid';
+import SubscribeSection from './components/Subscribe';
+import Testimonials from './components/Testimonial';
 
+function App() {
+  return(
+    <div className='App'> 
+    <Navbar />
+    <IntroComponent />
+    
+    <SeedGrid />
+    <SeedFeatures />
+    <Testimonials />
+    <SubscribeSection />
+    <Footer />
+  </div>
+  )
+}
 export default App;
